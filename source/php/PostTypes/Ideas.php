78096869f9e77d7ccf3b304f0aac89c23cb4b05b
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-11-09 16:05:11 +0100

    Idea post type

---
+++ source/php/PostTypes/Ideas.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace IdeaManager\PostTypes;
+
+class Ideas extends \IdeaManager\Entity\CustomPostType
+{
+    public function __construct()
+    {
+        parent::__construct(
+            __('Idea', 'idea-manager'),
+            __('Ideas', 'idea-manager'),
+            'idea',
+            array(
+                'menu_icon'     => 'dashicons-lightbulb',
+                'hierarchical'  => true,
+                'supports'      => array('title', 'revisions', 'editor', 'thumbnail'),
+            )
+        );
+
+        // Taxonomies
+        $this->addTaxonomy(
+            __('Status', 'idea-manager'),
+            __('Statuses', 'idea-manager'),
+            'idea_statuses'
+        );
+    }
+}
