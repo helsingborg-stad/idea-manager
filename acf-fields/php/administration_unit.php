91b4bc1f27b6901a923e5c9891ad0e8fda6fd713
Jonatan Hanson <jonatan.hanson@hiq.se>
2017-11-24 15:41:34 +0100

    Exported ACF field groups

---
+++ acf-fields/php/administration_unit.php
@@ -0,0 +1,54 @@
+<?php 
+
+if (function_exists('acf_add_local_field_group')) {
+    acf_add_local_field_group(array(
+    'key' => 'group_5a134bb83af1a',
+    'title' => 'Administration unit',
+    'fields' => array(
+        0 => array(
+            'key' => 'field_5a134bb84088b',
+            'label' => __('Administration unit', 'idea-manager'),
+            'name' => 'administration_unit',
+            'type' => 'taxonomy',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'taxonomy' => 'idea_administration_units',
+            'field_type' => 'radio',
+            'allow_null' => 0,
+            'add_term' => 1,
+            'save_terms' => 1,
+            'load_terms' => 1,
+            'return_format' => 'id',
+            'multiple' => 0,
+        ),
+    ),
+    'location' => array(
+        0 => array(
+            0 => array(
+                'param' => 'post_type',
+                'operator' => '==',
+                'value' => 'idea',
+            ),
+            1 => array(
+                'param' => 'current_user_role',
+                'operator' => '==',
+                'value' => 'administrator',
+            ),
+        ),
+    ),
+    'menu_order' => 0,
+    'position' => 'side',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => 1,
+    'description' => '',
+));
+}
