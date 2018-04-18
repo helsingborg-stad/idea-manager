c61396ee93f88e3f7894f224aed45e91a23085a5
Jonatan Hanson <jonatan.hanson@hiq.se>
2018-04-18 16:37:53 +0200

    Rewrote cluster map, saves locations to cache

--- source/js/front/map-cluster.js
+++ source/js/front/map-cluster.js
@@ -4,14 +4,18 @@ IdeaManager.Idea = IdeaManager.Idea || {};
 IdeaManager.Idea.ideaCluster = (function ($) {
 
     function ideaCluster() {
-        if (ideaManager.cluster.locations === undefined || ideaManager.cluster.locations.length == 0) {
-            return;
-        }
+        $(function() {
+            if (!$('#idea-cluster-map').length) {
+                return;
+            }
+            this.init();
+        }.bind(this));
+    }
 
-        var center = new google.maps.LatLng(56.046467, 12.694512);
-            mapOptions = {
+    ideaCluster.prototype.init = function() {
+        var mapOptions = {
             zoom: 12,
-            center: center,
+            center: new google.maps.LatLng(56.046467, 12.694512),
             panControl: false,
             zoomControl: true,
             mapTypeControl: false,
@@ -22,8 +26,10 @@ IdeaManager.Idea.ideaCluster = (function ($) {
             },
             map = new google.maps.Map(document.getElementById('idea-cluster-map'), mapOptions),
             geocoder = new google.maps.Geocoder(),
-            iw = new google.maps.InfoWindow({maxWidth: 330});
+            iw = new google.maps.InfoWindow({maxWidth: 330}),
+            bounds = new google.maps.LatLngBounds();
 
+        // Close info window on click
         function iwClose() { iw.close(); }
         google.maps.event.addListener(map, 'click', iwClose);
         var oms = new OverlappingMarkerSpiderfier(map, {
@@ -31,21 +37,21 @@ IdeaManager.Idea.ideaCluster = (function ($) {
             markersWontHide: true,
         });
 
-        // Make a cluster with the markers from the array
+        // Create cluster
         var markerCluster = new MarkerClusterer(map, [], { imagePath: ideaManager.cluster.iconPath, zoomOnClick: true, maxZoom: 17, gridSize: 20 });
         // Limits the zoom level
         google.maps.event.addListener(map, 'zoom_changed',
             function() {
                 if (map.getZoom() > 18) {
-                map.setZoom(18);
-            };
+                    map.setZoom(18);
+                }
         });
 
-        ideaManager.cluster.locations.map(function(location, i) {
-            geocoder.geocode({'address': location.address}, function(results, status) {
-                if (status == 'OK') {
+        $.when(this.getLocations()).then(function(data, textStatus, jqXHR) {
+            if (textStatus === 'success' && Array.isArray(data)) {
+                data.map(function(location, i) {
                     var marker = new google.maps.Marker({
-                        position: results[0].geometry.location,
+                        position: new google.maps.LatLng(location.coordinates)
                     });
 
                     var html = '<h3>' + location.title + '</h3><p>' + location.excerpt + '</p><br><a target="_top" class="btn btn-md btn-primary" href="' + location.permalink + '">' + ideaManager.readMore + '</a>';
@@ -59,15 +65,25 @@ IdeaManager.Idea.ideaCluster = (function ($) {
 
                     // Add marker to cluster
                     markerCluster.addMarker(marker, true);
-                    markerCluster.redraw();
-                }
-            });
-        });
+                    // Extend the bounds to include each marker's position
+                    bounds.extend(marker.position);
+                });
 
-        // for debugging/exploratory use in console
-        window.map = map;
-        window.oms = oms;
-    }
+                markerCluster.redraw();
+                map.panToBounds(bounds);
+            }
+        });
+    };
+
+    ideaCluster.prototype.getLocations = function() {
+        return $.ajax({
+            url: ajaxurl,
+            type: 'post',
+            data: {
+                action : 'idea_locations'
+            }
+        });
+    };
 
     return new ideaCluster();
 
--- source/php/Shortcode.php
+++ source/php/Shortcode.php
@@ -4,11 +4,12 @@ namespace IdeaManager;
 
 class Shortcode
 {
-	private $locations;
+    private $locations;
 
     public function __construct()
     {
         add_action('init', array($this, 'register_shortcodes'));
+        add_action('wp_ajax_idea_locations', array($this, 'getIdeaLocations'));
     }
 
     public function register_shortcodes()
@@ -18,59 +19,89 @@ class Shortcode
 
     public function ideaMap()
     {
-        if (!defined('G_GEOCODE_KEY')) {
-            echo "Define constant 'G_GEOCODE_KEY' to continue.";
+        if (!defined('G_GEOCODE_SERVER_KEY')) {
+            error_log("Define constant 'G_GEOCODE_SERVER_KEY' to continue.");
             return;
         }
 
-        $result = array();
-        $args = array(
-			'posts_per_page'   => -1,
-            'post_type'        => 'idea',
-            'post_status'      => 'publish',
-        );
-        $ideas = get_posts($args);
+        $this->shortcodeScripts();
+        echo '<div class="idea-cluster"><div id="idea-cluster-map"></div></div>';
+    }
 
-        if (!empty($ideas)) {
-            foreach ($ideas as $idea) {
-                $formData = get_post_meta($idea->ID, 'form-data', true);
-                $senderAddress = $formData['adress'] ?? $formData['address'] ?? null;
-                if (is_array($senderAddress) && !empty($senderAddress)) {
-                    $senderAddress = implode(',', $senderAddress);
-                    $result[] = array(
-                        'title' 	=> $idea->post_title,
-                        'address' 	=> $senderAddress,
-                   		'excerpt' 	=> wp_trim_words($idea->post_content, 30),
-                        'permalink' => get_permalink($idea->ID)
-                    );
+    public function getIdeaLocations()
+    {
+        if (!$locations = wp_cache_get('idea_locations')) {
+            ignore_user_abort();
+
+            $locations = array();
+            $args = array(
+                'posts_per_page'   => -1,
+                'post_type'        => 'idea',
+                'post_status'      => 'publish',
+            );
+            $ideas = get_posts($args);
+
+            if (!empty($ideas)) {
+                foreach ($ideas as $key => $idea) {
+                    // Pause one second to avoid 'OVER_QUERY_LIMIT' of 50 requests/sec
+                    if (($key + 1) % 49 == 0) {
+                        sleep(1);
+                    }
+
+                    $formData = get_post_meta($idea->ID, 'form-data', true);
+                    $senderAddress = $formData['adress'] ?? $formData['address'] ?? null;
+                    if (is_array($senderAddress) && !empty($senderAddress)) {
+                        $senderAddress = implode(',', $senderAddress);
+                        $coordinates = $this->getCoordinates($senderAddress);
+                        if (!empty($coordinates)) {
+                            $locations[] = array(
+                                'title'      => $idea->post_title,
+                                'address'      => $senderAddress,
+                                   'excerpt'      => wp_trim_words($idea->post_content, 30),
+                                'permalink'   => get_permalink($idea->ID),
+                                'coordinates' => $coordinates
+                            );
+                        }
+                    }
                 }
             }
+
+            // Save in cache for 2 weeks
+            wp_cache_add('idea_locations', $locations, '', 1209600);
         }
 
-        // Display map
-        if (!empty($result)) {
-        	$this->locations = $result;
-        	$this->shortcodeScripts();
-        	echo '<div class="idea-cluster"><div id="idea-cluster-map"></div></div>';
+        wp_send_json($locations);
+    }
+
+    public function getCoordinates($address)
+    {
+        $url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' . urlencode($address) . '&key=' . G_GEOCODE_SERVER_KEY;
+        $data = json_decode(file_get_contents($url));
+        $coordinates = array();
+
+        if (isset($data->status) && $data->status == 'OK') {
+            $coordinates['lat'] = $data->results[0]->geometry->location->lat;
+            $coordinates['lng'] = $data->results[0]->geometry->location->lng;
         }
+
+        return $coordinates;
     }
 
     public function shortcodeScripts()
     {
-		add_action('wp_enqueue_scripts', function() {
-			wp_enqueue_script('marker-clusterer', IDEAMANAGER_URL  . '/source/js/vendor/MarkerClusterer.min.js', array(), '', true);
-			wp_enqueue_script('marker-spiderfier', IDEAMANAGER_URL  . '/source/js/vendor/OverlappingMarkerSpiderfier.min.js', array(), '', true);
+        add_action('wp_enqueue_scripts', function () {
+            wp_enqueue_script('marker-clusterer', IDEAMANAGER_URL  . '/source/js/vendor/MarkerClusterer.min.js', array(), '', true);
+            wp_enqueue_script('marker-spiderfier', IDEAMANAGER_URL  . '/source/js/vendor/OverlappingMarkerSpiderfier.min.js', array(), '', true);
 
-	    	wp_enqueue_script('idea-manager');
-	    	wp_enqueue_script('google-maps-api');
-	    	wp_enqueue_style('idea-manager');
-	    	wp_localize_script('idea-manager', 'ideaManager', array(
-	    		'cluster' => array(
-	    			'locations' => $this->locations,
-	    			'iconPath' => IDEAMANAGER_URL . '/source/assets/images/'
-	    		),
-	    		'readMore' => __('Read more', 'idea-manager')
-			));
-		});
+            wp_enqueue_script('idea-manager');
+            wp_enqueue_script('google-maps-api');
+            wp_enqueue_style('idea-manager');
+            wp_localize_script('idea-manager', 'ideaManager', array(
+                'cluster' => array(
+                    'iconPath' => IDEAMANAGER_URL . '/source/assets/images/'
+                ),
+                'readMore' => __('Read more', 'idea-manager')
+            ));
+        });
     }
 }
